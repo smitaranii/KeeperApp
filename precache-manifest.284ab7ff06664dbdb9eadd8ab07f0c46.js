05d275f91049dc0fa22b9c4e575da82ce04ea056
smitarani <smitarani2002sumi@gmail.com>
2021-06-06 08:49:41 +0530

    Updates

---
+++ precache-manifest.284ab7ff06664dbdb9eadd8ab07f0c46.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d9ebd27de44007e8f48ff4ef2917e61",
+    "url": "/KeeperApp/index.html"
+  },
+  {
+    "revision": "e8b244af8038183c0782",
+    "url": "/KeeperApp/static/js/2.02b41491.chunk.js"
+  },
+  {
+    "revision": "c7333b0ff0b352e3c04d",
+    "url": "/KeeperApp/static/js/main.677b3740.chunk.js"
+  },
+  {
+    "revision": "f87f7f290d4c0085841d",
+    "url": "/KeeperApp/static/js/runtime-main.9f8bc1f0.js"
+  }
+]);
